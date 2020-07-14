d68377b8f143697d5ece05fbc8ce0cc4b75871cd
Onyekachi Ezeoke <ezeokeonyekachi@gmail.com>
2020-07-14 11:15:37 +0100

    add http method enum

---
+++ GitHub/Source/Data/Service/HTTPMethod/HTTPMethod.swift
@@ -0,0 +1,14 @@
+//
+//  HTTPMethod.swift
+//  GitHub
+//
+//  Created by Onyekachi Ezeoke on 14/07/2020.
+//  Copyright © 2020 Onyekachi Ezeoke. All rights reserved.
+//
+
+import Foundation
+
+public enum HTTPMethod: String {
+    case get
+    case post
+}
