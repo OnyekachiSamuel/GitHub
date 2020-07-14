e289b2d9e7ae505559e7034f2f299c51770469a7
Onyekachi Ezeoke <ezeokeonyekachi@gmail.com>
2020-07-14 13:11:27 +0100

    implement logic for displaying commit autors in tableview

--- GitHub/Source/App/Screens/Home/ViewController/HomeController.swift
+++ GitHub/Source/App/Screens/Home/ViewController/HomeController.swift
@@ -8,11 +8,44 @@
 
 import UIKit
 
-class HomeController: UIViewController {
+class HomeController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        setupView()
+        viewModel.completion = { [weak self] (commits, error) in
+            guard let self = self else { return }
+            self.commitAuthors = commits
+            self.tableView.reloadData()
+        }
+        viewModel.fetchCommits()
+    }
+    
+    // MARK: Private Instance Properties
+    
+    private let viewModel = HomeViewModel()
+    private var commitAuthors: [CommitAuthor] = []
+    
+    // MARK: Private Instance Method
+    
+    private func setupView() {
+        tableView.register(HomeCell.self, forCellReuseIdentifier: HomeCell.reuseIdentifier)
+        tableView.tableFooterView = UIView()
     }
 }
+
+extension HomeController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return commitAuthors.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: HomeCell.reuseIdentifier, for: indexPath) as! HomeCell
+        let commitAuthor = commitAuthors[indexPath.row]
+        cell.viewModel = HomeCellViewModel(commitAuthor: commitAuthor)
+        return cell
+    }
+}
+
+
 
